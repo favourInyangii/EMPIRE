b3a57465b48253d859ddb38a99d7dbffec7e0db2
favour ololade <ololadefavourajayi@gmail.com>
2023-10-24 15:31:56 +0100

    Using external js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let conc
+let conc2
+console.log("I am amazing");
+alert("the world is ending")
+conc= confirm("This site is Pg 18, do you want to proceed?")
+conc2= prompt("This site is Pg 18, do you want to proceed?")
+console.log(conc);
+console.log(conc2);
